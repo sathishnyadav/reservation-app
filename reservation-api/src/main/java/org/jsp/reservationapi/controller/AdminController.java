9515de3d368e3823eb06abc5b614d1c31d9eeb45
sathishnyadav <sathish.n.yadav@gmail.com>
2024-06-04 18:13:22 +0530

    Modified

--- reservation-api/src/main/java/org/jsp/reservationapi/controller/AdminController.java
+++ reservation-api/src/main/java/org/jsp/reservationapi/controller/AdminController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.Valid;
-
 @RestController
 @RequestMapping("/api/admins")
 public class AdminController {
